e3de61772c46cb42475345470c1cd453328967d3
dejosemario <josemariaodumodu@gmail.com>
2024-09-12 23:18:10 +0100

    fix: setup the app module env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+pt
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { UrlModule } from './url/url.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { QrcodeModule } from './qrcode/qrcode.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    RedisModule.forRoot({
+      type: 'single',
+      url: process.env.REDIS_URL,
+    }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    UrlModule, // Handles URL shortening logic
+    AnalyticsModule, // Tracks and provides analytics
+    QrcodeModule, // QR code generation
+  ],
 })
 export class AppModule {}
